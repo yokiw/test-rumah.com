196ef88610e5ab02716bacb4eeb8a2cb44cd3499
yokiw <yoki.wirahadi@gmail.com>
2014-01-30 21:58:58 +0700

    task1 - 2

--- task1.php
+++ task1.php
@@ -1,9 +1,11 @@
 <?php
 
-testPrint(12, 16);
+testPrint(4, 11);
 
 function testPrint($start, $end) {
-    
+
+    $c_int = true;
+
     for ($i = $start; $i <= $end; $i++) {
         if ($i % 3 == 0 || $i % 5 == 0) {
             if ($i % 3 == 0) {
@@ -12,8 +14,14 @@ function testPrint($start, $end) {
             if ($i % 5 == 0) {
                 echo 'Buzz';
             }
+            $c_int = false;
         } else {
-            echo $i;
+            if ($c_int) {
+                echo $i;
+            } else {
+                echo 'Bazz';
+            }
+            $c_int = true;
         }
         echo ' ';
     }
